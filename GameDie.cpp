76f4c6ded99201e532319adcba7cb36413eead47
Sathya <svvsathyanarayanan@gmail.com>
2023-02-21 10:11:53 -0800

    fix linting

--- GameDie.cpp
+++ GameDie.cpp
@@ -16,7 +16,7 @@ GameDie::GameDie() {
 // overloaded constructor
 GameDie::GameDie(unsigned int num) {
     srand(time(NULL));
-    if ( num == 0 ) {
+    if (num == 0) {
         roll_counter.resize(FACES);
     } else {
         roll_counter.resize(num);
@@ -29,13 +29,14 @@ GameDie::GameDie(unsigned int num) {
 // generate a random number between 1-n where n is the counter size
 // (inclusive) and return it
 int GameDie::roll() {
-    int roll = rand_r() % roll_counter.size();
+    unsigned int seed = 2;
+    int roll = rand_r(&seed) % roll_counter.size();
     roll_counter[roll]++;
     return roll + 1;
 }
 
 // return the count of how many times each face has been rolled, as a vector
 // where each face's count is at index face-1 (i.e. Face 1 is at index 0)
-vector <int> GameDie::get_distribution() {
+vector<int> GameDie::get_distribution() {
     return roll_counter;
 }
