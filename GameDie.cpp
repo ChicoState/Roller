d36c35fc3064f95706f817d65f7ee7cc4e71760d
Chaitanya <45332082+chetan2298@users.noreply.github.com>
2023-02-21 10:25:00 -0800

    Fixed styling and Lint Issues. Fix #1

--- GameDie.cpp
+++ GameDie.cpp
@@ -5,18 +5,19 @@
 #include <ctime>
 
 // class constructor that seeds the random number generator
-GameDie::GameDie() {
+GameDie::GameDie(){
     srand(time(NULL));
     roll_counter.resize(FACES);
 
     for (int i = 0; i < FACES; i++ )
-      roll_counter[i] = 0;
+        roll_counter[i] = 0;
 }
 
 // overloaded constructor
 GameDie::GameDie(unsigned int num) {
     srand(time(NULL));
-    if ( num == 0 ) {
+    if ( num == 0)
+    {
         roll_counter.resize(FACES);
     } else {
         roll_counter.resize(num);
@@ -28,14 +29,16 @@ GameDie::GameDie(unsigned int num) {
 
 // generate a random number between 1-n where n is the counter size
 // (inclusive) and return it
-int GameDie::roll() {
-    int roll = rand_r() % roll_counter.size();
+int GameDie::roll()
+{
+    int roll = rand() % roll_counter.size();
     roll_counter[roll]++;
     return roll + 1;
 }
 
 // return the count of how many times each face has been rolled, as a vector
 // where each face's count is at index face-1 (i.e. Face 1 is at index 0)
-vector <int> GameDie::get_distribution() {
+vector<int> GameDie::get_distribution()
+{
     return roll_counter;
-}
+}test
